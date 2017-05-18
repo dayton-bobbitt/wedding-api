36ef4c703760be559ad97f35c5eda81acf5ab75b
dbobbitt <dayton.bobbitt@gmail.com>
2017-05-17 20:33:35 -0400

    Check for cookie existence using cookie name from config

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const sassMiddleware = require('node-sass-middleware');
 const config = require('config');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,7 +36,7 @@ app.use('/api/guest/rsvp', rsvpGuest);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -56,7 +56,7 @@ app.use(function(err, req, res, next) {
  * Create cookie if header contains event code
  */
 function validateEventKey(req, res, next) {
-  if (typeof req.cookies.weddingEventKey === 'undefined') {
+  if (typeof req.cookies[config.get('cookieName')] === 'undefined') {
 
     // Set cookie if eventkey is valid
     const eventKey = req.headers.eventkey;
