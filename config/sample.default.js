58dff62fd4f9b6c242270e8c989ce3304f3b8a23
dbobbitt <dayton.bobbitt@gmail.com>
2017-05-22 19:40:52 -0400

    Update sample config

--- config/sample.default.js
+++ config/sample.default.js
@@ -2,5 +2,18 @@ module.exports = {
     cookieName: '',
     cookieValue: '',
     eventKey: '',
-    weddingDate: new Date()
+    weddingDate: new Date(),
+    details: {
+        msg: '',
+        when: '',
+        where: ''
+    },
+    connection: {
+        host: '',
+        user: '',
+        password: '',
+        database: ''
+    },
+    queryTemplate: '',
+    contactEmail: ''
 };
